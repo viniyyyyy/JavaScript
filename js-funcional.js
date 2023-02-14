2645f567a0aca82a7b123459905e81906584b2bb
unknown <you@example.com>
2023-02-14 11:13:39 -0300

    CalculoImc_Part1

---
+++ js-funcional.js
@@ -0,0 +1,43 @@
+
+const button = document.querySelector("#enviar");
+
+button.addEventListener("click", function (e) {
+    
+    e.preventDefault();
+
+// Objeto em JavaScript
+
+    var aluno = {
+    nome : document.getElementById("nome"),
+    idade : document.getElementById("idade"),
+    peso : document.getElementById("peso"),
+    altura : document.getElementById("altura"),
+    imc : document.getElementById("imc")
+    }
+    
+// Function
+
+function calculoImc(p, a) {
+    imc.value = p / (a*a);
+    return imc
+}
+
+function printar(n, i, p, a, im){
+    console.log(`nome: ${n} | idade: ${i} | peso: ${p} | altura: ${a} | imc: ${im}`)
+}
+
+// Descontrução
+
+var {nome:nomeObj, idade:idadeObj, peso:pesoObj, altura:alturaObj} = aluno
+
+printar(nomeObj.value, idadeObj.value, pesoObj.value, alturaObj.value, calculoImc(pesoObj.value, alturaObj.value).value)
+
+});
+
+// Não permitir editar input do imc
+
+const input = document.querySelector("#imc")
+
+console.log(input)
+
+input.disabled = true
